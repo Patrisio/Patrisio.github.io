4369c9e4cdb085969027877b89b56396b3444917
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-04-28 17:15:45 +0300

    bug fix

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import * as styles from './paragraph.module.css';
 
 export default function Paragraph({ text, size }) {
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import * as styles from './title.module.css';
 
 export default function Title({ text, size }) {
