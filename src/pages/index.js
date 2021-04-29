df33edfed4a8c5a13d118f68a9d3815e766463a2
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-04-29 18:28:37 +0300

    last changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = () => {
   const [isMobileDevice, setIsMobileDevice] = useState(false);
 
   useEffect(() => {
-    window.version = '1.0.6';
+    window.version = '1.0.7';
     const module = typeof window !== `undefined` ? require("../utils/detector") : null;
     const detect = module.default;
 
