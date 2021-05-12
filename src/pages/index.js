8b86589b67ecddf25d63ccde34e87bf283374c5e
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-05-12 14:34:00 +0300

    last

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,110 +47,114 @@ const IndexPage = () => {
     <Context.Provider value={{ lang, changeLang }}>
       <main style={{ fontFamily: '"JetBrains Mono", monospace', background: '#0C1201' }}>
         {
-          isLoading ?
-          <LoadingPage toggleLoading={toggleLoading}/> :
           <>
-            <div style={{ maxWidth: '1264px', margin: '0 auto' }}>
-              <Helmet>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet" />
-                <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-                <script defer src="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js"></script>
-              </Helmet>
-              <Header />
-              <MainScreen />
-              <Indicators id='aboutus' isMobileDevice={isMobileDevice}/>
+            <LoadingPage
+              toggleLoading={toggleLoading}
+              classNames={isLoading ? styles.activeLoadingPage : styles.hiddenLoadingPage}
+            />
+            <>
+              <div style={{ maxWidth: '1264px', margin: '0 auto' }}>
+                <Helmet>
+                  <link rel="preconnect" href="https://fonts.gstatic.com" />
+                  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet" />
+                  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                  <script defer src="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js"></script>
+                </Helmet>
+                <Header />
+                <MainScreen />
+                <Indicators id='aboutus' isMobileDevice={isMobileDevice}/>
 
-              <div className={styles.textBlockContainer}>
+                <div className={styles.textBlockContainer}>
+                  <TextBlock>
+                    <div className={styles.titleContainer}>
+                      <Title
+                        text='Мы дадим заряженных людей вокруг'
+                      />
+                    </div>
+                    <div className={styles.paragraphContainer} id='ourpros'>
+                      <Paragraph text='Мы делаем огромные объемы трафика в сутки, обгоняя многих конкурентов. Наш принцип - максимально инвестировать в инфраструктуру, брать лучших и надежных людей. Мы семья, которая помогает друг другу во всем.'/>
+                    </div>
+                  </TextBlock>
+                </div>
+
+                <Features isMobileDevice={isMobileDevice} />
+
+                <Waypoint
+                  onEnter={() => {
+                    if (!isActive) {
+                      const speed = isMobileDevice ? 10 : 20;
+                      type(
+                        `<h2 class='${styles.description}'>Мы готовы обсуждать сотрудничество с целыми командами, брать их под свое крыло и давать всю инфраструктуру для того, чтобы делать х10 от того, что они делают сейчас.</h2>`,
+                        0, false, '', 'index-1', speed);
+                    }
+                  }}
+                />
                 <TextBlock>
-                  <div className={styles.titleContainer}>
-                    <Title
-                      text='Мы дадим заряженных людей вокруг'
-                    />
-                  </div>
-                  <div className={styles.paragraphContainer} id='ourpros'>
-                    <Paragraph text='Мы делаем огромные объемы трафика в сутки, обгоняя многих конкурентов. Наш принцип - максимально инвестировать в инфраструктуру, брать лучших и надежных людей. Мы семья, которая помогает друг другу во всем.'/>
+                  <div className={styles.paragraph2}>
+                    <h2 className={styles.description} style={{ height: '75px' }} id='index-1' />
                   </div>
                 </TextBlock>
               </div>
 
-              <Features isMobileDevice={isMobileDevice} />
-
               <Waypoint
                 onEnter={() => {
                   if (!isActive) {
-                    const speed = isMobileDevice ? 10 : 20;
                     type(
-                      `<h2 class='${styles.description}'>Мы готовы обсуждать сотрудничество с целыми командами, брать их под свое крыло и давать всю инфраструктуру для того, чтобы делать х10 от того, что они делают сейчас.</h2>`,
-                      0, false, '', 'index-1', speed);
+                      `<div class='${styles.titleFlex}'>наши клиенты Нереально амбициозные</div>`,
+                      0, false, '', 'index-2', 20);
+                    type(
+                      `<h2 class='${styles.paragraphFlex}'>Мы отслеживаем более 1 миллиона коммерческих кликов через наших клиентов ежемесячно.</h2>`,
+                      0, false, '', 'index-3', 20); 
                   }
                 }}
               />
-              <TextBlock>
-                <div className={styles.paragraph2}>
-                  <h2 className={styles.description} style={{ height: '75px' }} id='index-1' />
+              <TextBlock flex>
+                <div className={styles.titleFlex} id='clients'>
+                  <div className={styles.titleFlex} id='index-2' />
+                </div>
+                <div className={styles.paragraphFlex}>
+                  <h2 className={styles.paragraphFlex} id='index-3' />
                 </div>
               </TextBlock>
-            </div>
 
-            <Waypoint
-              onEnter={() => {
-                if (!isActive) {
-                  type(
-                    `<div class='${styles.titleFlex}'>наши клиенты Нереально амбициозные</div>`,
-                    0, false, '', 'index-2', 20);
-                  type(
-                    `<h2 class='${styles.paragraphFlex}'>Мы отслеживаем более 1 миллиона коммерческих кликов через наших клиентов ежемесячно.</h2>`,
-                    0, false, '', 'index-3', 20); 
-                }
-              }}
-            />
-            <TextBlock flex>
-              <div className={styles.titleFlex} id='clients'>
-                <div className={styles.titleFlex} id='index-2' />
-              </div>
-              <div className={styles.paragraphFlex}>
-                <h2 className={styles.paragraphFlex} id='index-3' />
-              </div>
-            </TextBlock>
+              <InfiniteSlider />
 
-            <InfiniteSlider />
+              <Sources/>
 
-            <Sources/>
+              <Waypoint
+                onEnter={() => {
+                  toggleActive(true);
+                }}
+                bottomOffset={isMobileDevice ? 0 : 500}
+              />
 
-            <Waypoint
-              onEnter={() => {
-                toggleActive(true);
-              }}
-              bottomOffset={isMobileDevice ? 0 : 500}
-            />
+              <TextBlock>
+                <div className={`
+                  ${styles.title3}
+                  ${isActive && styles.active}
+                `}>
+                  <Title
+                    text='идеальное решение для вашего бизнеса'
+                  />
+                </div>
+                <div className={`
+                  ${styles.paragraph3}
+                  ${isActive && styles.active}
+                `}>
+                  <Paragraph text='Syndicate Group - это медиабаинг, заряженный на результат. Мы работаем только с качественными источниками трафика, которые приносят максимальную выгоду нашим клиентам.' />
+                </div>
+                <div className={styles.mobileParagraphs}>
+                  <Paragraph text='Syndicate Group - это медиабаинг, заряженный на результат.'/>
+                  <Paragraph text='Мы работаем только с качественными источниками трафика, которые приносят максимальную выгоду нашим клиентам.'/>
+                </div>
+              </TextBlock>
 
-            <TextBlock>
-              <div className={`
-                ${styles.title3}
-                ${isActive && styles.active}
-              `}>
-                <Title
-                  text='идеальное решение для вашего бизнеса'
-                />
-              </div>
-              <div className={`
-                ${styles.paragraph3}
-                ${isActive && styles.active}
-              `}>
-                <Paragraph text='Syndicate Group - это медиабаинг, заряженный на результат. Мы работаем только с качественными источниками трафика, которые приносят максимальную выгоду нашим клиентам.' />
-              </div>
-              <div className={styles.mobileParagraphs}>
-                <Paragraph text='Syndicate Group - это медиабаинг, заряженный на результат.'/>
-                <Paragraph text='Мы работаем только с качественными источниками трафика, которые приносят максимальную выгоду нашим клиентам.'/>
-              </div>
-            </TextBlock>
+              <Charts id='cases' />
 
-            <Charts id='cases' />
+              <Form id='form' isMobileDevice={isMobileDevice}/>
 
-            <Form id='form' isMobileDevice={isMobileDevice}/>
-
-            <Footer />
+              <Footer />
+            </>
           </>
         }
       </main>
